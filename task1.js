acd42b5d1b0606728b762d256cc13cbdef795217
Andrii-Mykhanyshyn <andriymykhanyshyn@gmail.com>
2025-01-31 11:54:43 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -5,3 +5,17 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const usersList = document.querySelector(".usersList");
+
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
+
+        usersList.innerHTML = users.map(user => `<li>${user.name}</li>`).join("");
+    } catch (error) {
+        console.error("Error fetching users:", error);
+    }
+});
